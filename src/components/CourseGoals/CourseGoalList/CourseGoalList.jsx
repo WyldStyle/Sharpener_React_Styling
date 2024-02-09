ee3a81d077079b389ff3a6a2b171cc478702eaa0
WyldCode <uj.wyldstyle@gmail.com>
2024-02-09 23:19:08 +0530

    'Red_Add_button_modified'

--- src/components/CourseGoals/CourseGoalList/CourseGoalList.jsx
+++ src/components/CourseGoals/CourseGoalList/CourseGoalList.jsx
@@ -1,25 +1,8 @@
 // import React from 'react';
 
 import CourseGoalItem from '../CourseGoalItem/CourseGoalItem.jsx'
-// import './CourseGoalList.css';
+import './CourseGoalList.css';
 
-// const CourseGoalList = props => {
-//   return (
-//     <ul className="goal-list">
-//       {props.items.map(goal => (
-//         <CourseGoalItem
-//           key={goal.id}
-//           id={goal.id}
-//           onDelete={props.onDeleteItem}
-//         >
-//           {goal.text}
-//         </CourseGoalItem>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// export default CourseGoalList;
 
 const CourseGoalList = ({ courseGoalArrApp, setCourseGoalArr }) => {
   // console.log('arrin List', courseGoalArrApp, courseGoalArrApp[0]);
